76cec3a7502938004d3a4847e378ff69d8732f22
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-25 22:22:13 +0400

    [feat] Add early version of custom login page

---
+++ src/components/auth/index.jsx
@@ -0,0 +1,1 @@
+export { default as MSignIn } from "./MSignIn";
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Auth } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { MSignIn } from "../auth";
+import { Authenticator } from "aws-amplify-react";
+
+// "@aws-amplify/ui-react";
+
+// withAuthenticator,
 
 interface Props {
   user: {
@@ -10,12 +15,19 @@ interface Props {
 
 interface State {}
 
+const CustomAuthenticator = () => (
+  <Authenticator hideDefault>
+    <MSignIn />
+  </Authenticator>
+);
+
 class Login extends Component<Props, State> {
   render() {
     const { user } = this.props;
     return (
       <>
-        {!user && Auth.federatedSignIn()}
+        {/*Auth.federatedSignIn() */}
+        {!user && <CustomAuthenticator />}
         {user && (
           <div>
             You are signed in as <i>{user.username}</i>.
