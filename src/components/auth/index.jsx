712fd239af258eb3f26758483dc1235a19a6a517
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-29 22:09:56 +0400

    [feat] Added sign up confirmation functionality

--- src/components/auth/index.jsx
+++ src/components/auth/index.jsx
@@ -2,3 +2,4 @@ export { default as MSignIn } from "./MSignIn";
 export { default as MSignOut } from "./MSignOut";
 export { default as MSignUp } from "./MSignUp";
 export { default as MConfirmSignIn } from "./MConfirmSignIn";
+export { default as MConfirmSignUp } from "./MConfirmSignUp";
