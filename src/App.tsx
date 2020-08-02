a3f931b136b72f9ac784eea2edf56e90914354d2
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-02 17:48:58 +0400

    [feat] Added AmplifyAuthenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 import Navigator from "./components/Navigator";
 import Amplify, { Auth } from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
 import Main from "./components/Main";
 
+const federated_data = {
+  facebookAppId: "286828136089750",
+  googleClientId:
+    "965464288608-eq384s4i76u0tq8kcmrsc9acrff9k75j.apps.googleusercontent.com",
+};
+
 function App() {
+  return (
+    <AmplifyAuthenticator>
+      {/*federated={federated_data}*/}
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          {
+            type: "password",
+            label: "Custom Password Label",
+            placeholder: "custom password placeholder",
+          },
+          { type: "email" },
+        ]}
+      />
+      <Navigator />
+      <Main />
+    </AmplifyAuthenticator>
+  );
+}
+
+export default App;
+
+/*function App() {
   return (
     <>
       <Navigator />
@@ -12,4 +43,6 @@ function App() {
   );
 }
 
-export default App;
+
+export default withAuthenticator(App);
+*/
