30b715131929bef020bb5515cbd8643726d83fdc
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-02 17:51:10 +0400

    [feat] No more using custom auth for the moment

--- src/components/auth/index.jsx
+++ src/components/auth/index.jsx
@@ -1,5 +1,5 @@
-export { default as MSignIn } from "./MSignIn";
+/*export { default as MSignIn } from "./MSignIn";
 export { default as MSignOut } from "./MSignOut";
 export { default as MSignUp } from "./MSignUp";
 export { default as MConfirmSignIn } from "./MConfirmSignIn";
-export { default as MConfirmSignUp } from "./MConfirmSignUp";
+export { default as MConfirmSignUp } from "./MConfirmSignUp";*/
